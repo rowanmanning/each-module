ddb417dd25354aa7a62281c82fc01a8d7b59dd8f
Rowan Manning <info@rowanmanning.com>
2014-04-07 20:53:02 +0100

    Write pending tests

--- test/each-module.js
+++ test/each-module.js
@@ -1,1 +1,35 @@
+/* jshint maxlen: false, maxstatements: false */
+/* global afterEach, beforeEach, describe, it */
 'use strict';
+
+var assert = require('proclaim');
+var mockery = require('mockery');
+var sinon = require('sinon');
+
+describe('each-module', function () {
+    var eachModule;
+
+    beforeEach(function () {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false,
+            warnOnReplace: false
+        });
+        eachModule = require('../lib/each-module');
+    });
+
+    afterEach(function () {
+        mockery.disable();
+    });
+
+    it('should be a function');
+
+    it('should error if called with an invalid `path` argument');
+
+    it('should error if called with an invalid `fn` argument');
+
+    it('should glob for node modules in the expected directory');
+
+    it('should call `fn` for each file in the directory');
+
+});
