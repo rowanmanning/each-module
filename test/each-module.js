84838b875c634ddce905bbec9a206aca7c909ee3
Dominic Barnes <dominic@dbarnes.info>
2015-03-11 13:15:47 -0700

    adding json file support

--- test/each-module.js
+++ test/each-module.js
@@ -19,23 +19,26 @@ describe('each-module', function () {
         glob = {};
         mockery.registerMock('glob', glob);
         glob.sync = sinon.stub();
-        glob.sync.withArgs('foo/**/*.{js,coffee}').returns([
+        glob.sync.withArgs('foo/**/*.{js,json,coffee}').returns([
             'foo/bar.js',
             'foo/baz.js',
             'foo/qux/qux.js',
-            'foo/hello.coffee'
+            'foo/hello.coffee',
+            'foo/data.json'
         ]);
 
         mods = {
             bar: {bar: true},
             baz: {baz: true},
             qux: {qux: true},
-            hello: {hello: true}
+            hello: {hello: true},
+            data: {data: true}
         };
         mockery.registerMock('foo/bar.js', mods.bar);
         mockery.registerMock('foo/baz.js', mods.baz);
         mockery.registerMock('foo/qux/qux.js', mods.qux);
         mockery.registerMock('foo/hello.coffee', mods.hello);
+        mockery.registerMock('foo/data.json', mods.data);
 
         path = {};
         mockery.registerMock('path', path);
@@ -45,6 +48,7 @@ describe('each-module', function () {
         path.relative.withArgs('foo', 'foo/baz.js').returns('baz.js');
         path.relative.withArgs('foo', 'foo/qux/qux.js').returns('qux/qux.js');
         path.relative.withArgs('foo', 'foo/hello.coffee').returns('hello.coffee');
+        path.relative.withArgs('foo', 'foo/data.json').returns('data.json');
 
         eachModule = require('../lib/each-module');
     });
@@ -71,13 +75,13 @@ describe('each-module', function () {
 
     it('should glob for node modules in the expected directory', function () {
         eachModule('foo', function () {});
-        assert.strictEqual(glob.sync.withArgs('foo/**/*.{js,coffee}').callCount, 1);
+        assert.strictEqual(glob.sync.withArgs('foo/**/*.{js,json,coffee}').callCount, 1);
     });
 
     it('should call `fn` for each file in the directory', function () {
         var fn = sinon.spy();
         eachModule('foo', fn);
-        assert.strictEqual(fn.callCount, 4);
+        assert.strictEqual(fn.callCount, 5);
         assert.strictEqual(fn.getCall(0).args[0], 'bar');
         assert.strictEqual(fn.getCall(0).args[1], mods.bar);
         assert.strictEqual(fn.getCall(1).args[0], 'baz');
@@ -86,6 +90,8 @@ describe('each-module', function () {
         assert.strictEqual(fn.getCall(2).args[1], mods.qux);
         assert.strictEqual(fn.getCall(3).args[0], 'hello');
         assert.strictEqual(fn.getCall(3).args[1], mods.hello);
+        assert.strictEqual(fn.getCall(4).args[0], 'data');
+        assert.strictEqual(fn.getCall(4).args[1], mods.data);
     });
 
 });
